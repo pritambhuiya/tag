119c2c76309321563e90b8086c64b237d05b18f8
Pritam Bhuiya <99480050+pritambhuiya@users.noreply.github.com>
2022-05-31 13:31:28 +0530

    Created generatedPage function and wrote test for it.

--- src/tag.js
+++ src/tag.js
@@ -14,11 +14,24 @@ class Tag {
 
   toHtml() {
     const content = this.content || '';
-    const attributes = this.attributes || '';
+    const attributes = this.attributes ? ' ' + this.attributes : '';
 
     return `<${this.tag}${attributes}>${content}</${this.tag}>`;
   }
-
 }
 
+const generatePage = () => {
+  const linkTag = '<link rel="stylesheet" href="styles.css">';
+
+  const head = new Tag({ tag: 'head', content: linkTag });
+  const headTag = head.toHtml();
+
+  const body = new Tag({ tag: 'body', content: '' });
+  const bodyTag = body.toHtml();
+
+  const html = new Tag({ tag: 'html', content: headTag + bodyTag });
+  return html.toHtml();
+};
+
 exports.Tag = Tag;
+exports.generatePage = generatePage;
--- test/testTag.js
+++ test/testTag.js
@@ -1,4 +1,4 @@
-const { Tag } = require('../src/tag.js');
+const { Tag, generatePage } = require('../src/tag.js');
 
 const assert = require('assert');
 
@@ -29,8 +29,27 @@ describe('toHtml', () => {
     assert.strictEqual(head.toHtml(), '<head>content</head>');
   });
 
-  it('Should generate a tag without content', () => {
+  it('Should generate a tag with attributes', () => {
+    const head = new Tag({ tag: 'head', attributes: 'class="ok"' });
+    assert.strictEqual(head.toHtml(), '<head class="ok"></head>');
+  });
+
+  it('Should generate a tag with contents, attributes', () => {
+    const head = new Tag(
+      { tag: 'head', content: 'hi', attributes: 'class="ok"' });
+    assert.strictEqual(head.toHtml(), '<head class="ok">hi</head>');
+  });
+
+  it('Should generate a tag without content & attributes', () => {
     const head = new Tag({ tag: 'head', content: '' });
     assert.strictEqual(head.toHtml(), '<head></head>');
+  });
+});
+
+describe('generatePage', () => {
+  it('Should generate page with given contents', () => {
+    const expected = '<html><head><link rel="stylesheet"' +
+      ' href="styles.css"></head><body></body></html>';
+    assert.strictEqual(generatePage(), expected);
   });
 });
